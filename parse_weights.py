c97b8ae9dde49da4446834425ddf3fc84bb35287
HenryX <hx2209@columbia.edu>
2017-09-29 03:16:17 -0400

    modified parse_weight to include snps allele info

--- parse_weights.py
+++ parse_weights.py
@@ -1,7 +1,7 @@
 from rpy2.robjects import r, pandas2ri
 import numpy as np
 import os.path
-from scipy.sparse import coo_matrix
+from scipy.sparse import csr_matrix
 import sys
 
 ENET_INDEX = 2
@@ -12,17 +12,15 @@ def rload(filepath):
     wgtmat = np.array(r["wgt.matrix"])
     wgtmat_rows = np.array(r["rownames"](r["wgt.matrix"]))
     snps = np.array(r["snps"]).T
-
     # construct snp to index dict
     snp2index = {k: v for v, k in enumerate(wgtmat_rows)}
-
-    # return stuff
     return (wgtmat, snps, snp2index)
 
 def process_index_file(index_filepath):
     dirpath = os.path.dirname(index_filepath)
     expr_id_cache = {}
     snps_so_far = set()
+
     for row in np.genfromtxt(index_filepath, dtype="str", skip_header=1):
         rdata_relative_filepath, expr_id, chrom, p0, p1 = row
         wgtmat, snps, snp2index = rload(dirpath + "/" + rdata_relative_filepath)
@@ -33,24 +31,24 @@ def process_index_file(index_filepath):
             expr_id_cache[expr_id] = {"wgtmat": wgtmat, "snps": snps, "snp2index": snp2index,
                 "chrom": chrom, "p0": p0, "p1": p1, "nonzero_snps": nonzero_snps}
             snps_so_far.update(nonzero_snps) # this lets snps_so_far be the union of snps_so_far and the snps of snp2index.keys() with nonzero effects
+    
     snp2col = {k: v for v, k in enumerate(snps_so_far)}
     expr2row = {k: v for v, k in enumerate(expr_id_cache)}
     gen2expr_wgtmat = np.zeros((len(expr2row), len(snp2col)))
+    all_snps = np.empty((len(snp2col),), dtype=object)
     for expr_id in expr_id_cache:
         for snp in expr_id_cache[expr_id]["nonzero_snps"]:
             gen2expr_wgtmat[expr2row[expr_id], snp2col[snp]] = expr_id_cache[expr_id]["wgtmat"][expr_id_cache[expr_id]["snp2index"][snp], ENET_INDEX]
-    return (coo_matrix(gen2expr_wgtmat), expr2row, snp2col, snp)
+            all_snps[snp2col[snp]] = [expr_id_cache[expr_id]["snps"][expr_id_cache[expr_id]["snp2index"][snp]][i] for i in [1,2,4,5]]
 
-def save_gen2expr_wgtmat(filepath, gen2expr_wgtmat, expr2row, snp2col, snp):
-    np.savez_compressed(filepath, gen2expr_wgtmat=gen2expr_wgtmat, expr2row=expr2row, snp2col=snp2col, snp=snp)
+    return (csr_matrix(gen2expr_wgtmat), expr2row, snp2col, all_snps)
 
-def load_gen2expr_wgtmat(filepath):
-    loaded = np.load(filepath)
-    return (loaded["gen2expr_wgtmat"], loaded["expr2row"].item(), loaded["snp2col"].item())
+def save_gen2expr_wgtmat(filepath, gen2expr_wgtmat, expr2row, snp2col, snps):
+    np.savez_compressed(filepath, gen2expr_wgtmat=gen2expr_wgtmat, expr2row=expr2row, snp2col=snp2col, snps=snps)
 
 if __name__ == "__main__":
     index_filepath = sys.argv[1]
     save_filepath = sys.argv[2]
-    gen2expr_wgtmat, expr2row, snp2col, snp = process_index_file(index_filepath)
-    save_gen2expr_wgtmat(save_filepath, gen2expr_wgtmat, expr2row, snp2col, snp)
+    gen2expr_wgtmat, expr2row, snp2col, snps = process_index_file(index_filepath)
+    save_gen2expr_wgtmat(save_filepath, gen2expr_wgtmat, expr2row, snp2col, snps)
     
