3d42eac0849492df278bcaa81775ccf75a2c8a7c
HenryX <hx2209@columbia.edu>
2017-10-16 11:06:25 -0400

    adding simulation method for control group

--- lt_simulation.py
+++ lt_simulation.py
@@ -27,3 +27,27 @@ def get_genos_data_from_csv(genos_path):
 
 def get_freqs(genos):
 	return np.array([np.mean(genos[:,i])/2 for i in range(genos.shape[1])], dtype=np.float64);
+
+def simulate_individual(num, freqs):
+	num = int(num)
+	inds = np.empty([num, len(freqs)])
+	for i, p in enumerate(freqs):
+		sprobs = [(1-p)*(1-p), 2*p*(1-p), p*p]
+		inds[:,i] = np.random.choice(3,size=num,p=sprobs)
+	return inds
+
+def get_pred(gen2expr_wgtmat, snp2col, genos, snps, alpha):
+	expr = calc_expr.calc_expr_by_data(gen2expr_wgtmat, snp2col, genos, snps)
+	pred = expr.dot(alpha)
+	return (pred - np.mean(pred))/ np.std(pred)
+
+def get_controls(control_size, freqs, gen2expr_wgtmat, snp2col, snps, alpha, thresh=2):
+	control = []
+	while(len(control) < control_size):
+		inds = simulate_individual(control_size / 5, freqs)
+		pred = get_pred(gen2expr_wgtmat, snp2col, inds, snps, alpha)
+		for i, v in enumerate(pred):
+			if v < thresh:
+				control.append(inds[i])
+				print("Getting " + str(len(control) - 1) + "th control")
+	return np.array(control[0:control_size])
