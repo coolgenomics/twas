7fd0b68ac5cb60fdd2c5aa43f21e6bd1b41bc361
HenryX <hx2209@columbia.edu>
2017-10-16 11:06:36 -0400

    adding simulation method for case group

--- lt_simulation.py
+++ lt_simulation.py
@@ -51,3 +51,34 @@ def get_controls(control_size, freqs, gen2expr_wgtmat, snp2col, snps, alpha, thr
 				control.append(inds[i])
 				print("Getting " + str(len(control) - 1) + "th control")
 	return np.array(control[0:control_size])
+
+
+def get_cases(case_sizes, allele_props, freqs, gen2expr_wgtmat, snp2col, snps, alpha, thresh=2):
+	cases = []
+	nsnps = len(snps)
+	start = 0
+	for i, prop in enumerate(allele_props):
+		end = nsnps * prop + start
+		end = int(min(nsnps, end))
+		case = []
+		while(len(case) < case_sizes[i]):
+			inds = simulate_individual(case_sizes[i] * 5, freqs)
+			pred = get_pred(gen2expr_wgtmat, snp2col, inds[:,start:end], snps[start:end], alpha)
+			for k, v in enumerate(pred):
+				if v > thresh:
+					case.append(inds[k])
+					print("Getting " + str(len(case) - 1) + "th case")
+		cases.append(np.array(case[0:case_sizes[i]]))
+		start = end
+	return cases
+
+if __name__ == "__main__":
+	g2e_path = sys.argv[1]
+	alpha_path = sys.argv[2]
+	npz_path = sys.argv[3]
+	gen2expr_wgtmat, expr2row, snp2col, snps_ref, alpha = prepare_data(g2e_path, alpha_path)
+	sample, snps, genos = get_genos_data_from_npz(npz_path)
+	freqs = get_freqs(genos)
+	control = get_controls(4000, freqs, gen2expr_wgtmat, snp2col, snps, alpha)
+	cases = get_cases([2000,2000], [0.4, 0.4], freqs, gen2expr_wgtmat, snp2col, snps, alpha)
+	np.savez_compressed("test2/simulated.npz", cases=cases, control=control)
